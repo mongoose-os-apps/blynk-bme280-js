db68c77d963aec3f9bd36ee84d9a9281703bc189
Sergey Lyubka <sergey.lyubka@cesanta.com>
2017-08-11 09:50:51 +0100

    Import

---
+++ README.md
@@ -0,0 +1,31 @@
+# Using Blynk with Mongoose OS
+
+## Overview
+
+This example shows how to use MongooseOS with Blynk mobile framework.
+Go to device configuration and specify
+`blynk.auth` setting to your Blynk access token. Or, alternatively,
+run the following console command (from the terminal or "Terminal" tab in Web UI):
+
+```bash
+mos config-set blynk.auth=YOUR_TOKEN blynk.server="blynk-cloud.com:8442"
+```
+
+## Blynk configuration
+- Create a graph with virtual pin 1 (Graph of memory usage) limits min 0 - 50
+- Create a button with virtual pin 2 (Toggles led on/off)
+- Create a graph with virtual pin 3 (Graph of temperature) limits min -30 - 60
+- Create a graph with virtual pin 4 (Graph of humidity) limits min 0 - 100
+- Create a graph with virtual pin 5 (Graph of pressure) limits min 500 - 3000
+
+You can easily add your own handlers for Blynk virtual pins, in either C
+or JavaScript.
+
+## How to install this app
+
+- Install and start [mos tool](https://mongoose-os.com/software.html)
+- Switch to the Project page, find and import this app, build and flash it:
+
+<p align="center">
+  <img src="https://mongoose-os.com/images/app1.gif" width="75%">
+</p>
---
+++ fs/init.js
@@ -0,0 +1,49 @@
+load('api_arduino_bme280.js');
+load('api_gpio.js');
+load('api_blynk.js');
+load('api_sys.js');
+
+// Sensors address
+let sens_addr = 0x77;
+// Initialize Adafruit_BME280 library
+let bme = Adafruit_BME280.create();
+// Initialize the sensor
+if (bme.begin(sens_addr) === 0) {
+  print('Cant find a sensor');
+} else {
+  print('BME280 sensor found!');
+  print('Temperature:', bme.readTemperature(), '*C');
+  print('Humidity:', bme.readHumidity(), '%RH');
+  print('Pressure:', bme.readPressure(), 'hPa');
+}
+
+Blynk.setHandler(function(conn, cmd, pin, val, id) {
+  let ram = Sys.free_ram() / 1024;
+  let temperature = bme.readTemperature();
+  let humidity = bme.readHumidity();
+  let pressure = bme.readPressure();
+  if (cmd === 'vr') {
+  	// When reading any virtual pin, report free RAM in KB
+  	if (pin === 1) {
+  	 Blynk.virtualWrite(conn, pin, ram, id);
+  	}
+  	else if (pin === 3) {
+  	  Blynk.virtualWrite(conn, pin, temperature, id);
+  	}
+  	else if (pin === 4) {
+  	  Blynk.virtualWrite(conn, pin, humidity, id);
+  	}
+  	else if (pin === 5) {
+  	  Blynk.virtualWrite(conn, pin, pressure, id);
+  	}
+  	else {
+  	  print('BLYNK JS handler, unknown', ram, cmd, id, pin, val);
+  	}
+  } else if (cmd === 'vw') {
+  	// Writing to virtual pin translate to writing to physical pin
+    GPIO.set_mode(pin, GPIO.MODE_OUTPUT);
+    GPIO.write(pin, val);
+  } else {
+	  print('Unknown command', cmd, id, pin, val);
+  }
+}, null);
